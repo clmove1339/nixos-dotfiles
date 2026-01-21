ef9e8dabafd5e32f818cfc8a44e02b6efb5e9d96
clmove <clmove@xyecoc.com>
2026-01-21 21:26:58 +0300

    bullshit

--- home.nix
+++ home.nix
@@ -27,47 +27,141 @@
   programs.fastfetch = {
     enable = true;
     settings = {
-      logo = {
-        source = "nixos_small";
-        padding = { right = 1; };
-      };
       display = {
-        separator = "  ";
-        color = "cyan";
+        separator = " 󰑃  ";
+      };
+      logo = {
+        source = "Linux";
+        type = "builtin";
+        padding = {
+          top = 6;
+          left = 2;
+        };
       };
       modules = [
-        "title"
-        "separator"
+        "break"
+        {
+          type = "title";
+          color = {
+            user = "35";
+            host = "36";
+          };
+        }
+        {
+          type = "separator";
+          string = "▔";
+        }
         {
           type = "os";
-          key = "distro";
-          keyColor = "cyan";
+          key = " Distro";
+          keyColor = "yellow";
         }
         {
           type = "kernel";
-          key = "kernel";
-          keyColor = "cyan";
-        }
-        {
-          type = "uptime";
-          key = "uptime";
-          keyColor = "cyan";
+          key = "  ├─";
+          keyColor = "yellow";
         }
         {
           type = "packages";
-          key = "pkgs";
-          keyColor = "cyan";
+          key = "  ├─󰏖";
+          keyColor = "yellow";
         }
         {
           type = "shell";
-          key = "shell";
-          keyColor = "cyan";
+          key = "  └─";
+          keyColor = "yellow";
+        }
+        "break"
+        {
+          type = "wm";
+          key = " DE/WM";
+          keyColor = "blue";
+        }
+        {
+          type = "theme";
+          key = "  ├─󰉼";
+          keyColor = "blue";
+        }
+        {
+          type = "wmtheme";
+          key = "  ├─󰉼";
+          keyColor = "blue";
+        }
+        {
+          type = "icons";
+          key = "  ├─󰉋";
+          keyColor = "blue";
+        }
+        {
+          type = "cursor";
+          key = "  ├─󰳽";
+          keyColor = "blue";
+        }
+        {
+          type = "font";
+          key = "  ├─";
+          format = "{2}";
+          keyColor = "blue";
+        }
+        {
+          type = "terminal";
+          key = "  └─";
+          keyColor = "blue";
+        }
+        "break"
+        {
+          type = "host";
+          key = "󰌢 System";
+          keyColor = "green";
+        }
+        {
+          type = "board";
+          key = "󰌢 System";
+          keyColor = "green";
+          condition = {
+            succeeded = false;
+          };
+        }
+        {
+          type = "display";
+          key = "  ├─󰹑";
+          keyColor = "green";
+          compactType = "original-with-refresh-rate";
+        }
+        {
+          type = "cpu";
+          key = "  ├─";
+          keyColor = "green";
+        }
+        {
+          type = "gpu";
+          key = "  ├─";
+          format = "{2}";
+          keyColor = "green";
         }
         {
           type = "memory";
-          key = "memory";
-          keyColor = "cyan";
+          key = "  ├─󰘚";
+          keyColor = "green";
         }
+        {
+          type = "disk";
+          key = "  ├─󰋊";
+          folders = "/";
+          keyColor = "green";
+        }
+        {
+          type = "uptime";
+          key = "  └─󰔚";
+          keyColor = "green";
+        }
+        "break"
+        {
+          type = "colors";
+          symbol = "background";
+          paddingLeft = 9;
+        }
+        "break"
       ];
     };
   };
