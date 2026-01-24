bf35d8a240e6cca6b4b5231deead8b396af21403
clmove <clmove@xyecoc.com>
2026-01-24 23:51:03 +0300

    ai fixes..

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -7,10 +7,10 @@ let
   # Helper to generate directional bindings for both arrows and vim keys
   # directions = { key = direction; ... }
   directions = {
-    left = "l";
-    right = "r";
-    up = "u";
-    down = "d";
+    "left" = "l";
+    "right" = "r";
+    "up" = "u";
+    "down" = "d";
   };
 in
 {
@@ -56,8 +56,8 @@ in
         sensitivity = 0;
       };
 
-      bind = [
-        # General
+      bind = lib.flatten [
+        # Essentials
         "$mod, RETURN, exec, ${terminal}"
         "$mod, SPACE, exec, ${menu}"
         "$mod, C, killactive,"
@@ -65,30 +65,30 @@ in
         "$mod, V, togglefloating,"
         "$mod, F, fullscreen,"
 
-        # Special Workspace logic (0 -> 10)
-        (builtins.concatLists (builtins.genList (x:
+        # Workspace Navigation (Mouse/Arrows)
+        "$mod, mouse_down, workspace, e+1"
+        "$mod, mouse_up, workspace, e-1"
+        "$mod ALT, left, workspace, -1"
+        "$mod ALT, right, workspace, +1"
+
+        # Generate Workspace Bindings (Key 1-9 -> WS 1-9, Key 0 -> WS 10)
+        (map (n:
           let
-            ws = let c = (x + 1) / 10; in toString (x + 1 - (c * 10)); # Maps 0-9 to 1-10
-            key = toString x;
+            key = toString n;
+            ws = if n == 0 then "10" else key;
           in [
             "$mod, ${key}, workspace, ${ws}"
             "$mod SHIFT, ${key}, movetoworkspace, ${ws}"
           ]
-        ) 10))
+        ) [ 1 2 3 4 5 6 7 8 9 0 ])
 
-        # Mouse / Scroll workspace navigation
-        "$mod, mouse_down, workspace, e+1"
-        "$mod, mouse_up, workspace, e-1"
-        "$mod CTRL, left, workspace, -1"
-        "$mod CTRL, right, workspace, +1"
-      ]
-      ++ (lib.mapAttrsToList (key: dir: "$mod, ${key}, movefocus, ${dir}") directions)
-      ++ (lib.mapAttrsToList (key: dir: "$mod SHIFT, ${key}, movewindow, ${dir}") directions)
-      ++ (lib.mapAttrsToList (key: dir:
-          let
-            res = if dir == "l" then "-20 0" else if dir == "r" then "20 0"
-                  else if dir == "u" then "0 -20" else "0 20";
-          in "$mod ALT, ${key}, resizeactive, ${res}") directions);
+        # Directional Bindings (Focus, Move, Resize)
+        (lib.mapAttrsToList (key: dir: [
+          "$mod, ${key}, movefocus, ${dir}"
+          "$mod SHIFT, ${key}, movewindow, ${dir}"
+          "$mod CTRL, ${key}, resizeactive, ${if dir == "l" then "-20 0" else if dir == "r" then "20 0" else if dir == "u" then "0 -20" else "0 20"}"
+        ]) directions)
+      ];
 
       bindm = [
         "$mod, mouse:272, movewindow"
