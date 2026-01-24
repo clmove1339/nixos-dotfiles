71ace1b335d3d0c0f2ac68db7a324a8f9b9f6dfc
clmove <clmove@xyecoc.com>
2026-01-24 20:41:42 +0300

    rofi changes

--- modules/rofi/default.nix
+++ modules/rofi/default.nix
@@ -5,5 +5,17 @@
     theme = {
       "@theme" = "${./style.rasi}";
     };
+
+    plugins = [ pkgs.rofi-calc ];
+    extraConfig = {
+      modi = "drun,ssh,run";
+      display-drun = "   Apps ";
+      display-run = "   Run ";
+      display-ssh = " 󰌘 Ssh ";
+      drun-display-format = "{icon} {name}";
+      show-icons = true;
+      sidebar-mode = true;
+      terminal = "ghostty";
+    };
   };
 }
