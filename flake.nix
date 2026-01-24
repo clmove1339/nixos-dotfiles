a3174e3de28ee2b7e560e6ac8220e7806bd510fe
clmove <clmove@xyecoc.com>
2026-01-24 14:48:41 +0300

    updated flake, removed noctalia trash

--- flake.nix
+++ flake.nix
@@ -8,11 +8,6 @@
       url = "github:nix-community/home-manager?ref=release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
