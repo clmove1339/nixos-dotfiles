fb15075e3631e5b56b05264699092c6c842106a8
clmove <clmove@xyecoc.com>
2026-01-24 13:26:25 +0300

    this is bullshit...

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -11,6 +11,7 @@
   boot.kernelParams = [
     "quiet"
     "splash"
+    "tsc=reliable"
   ];
 
   boot.loader.systemd-boot.enable = true;
@@ -28,16 +29,27 @@
   users.users.clmove = {
     isNormalUser = true;
     shell = pkgs.zsh;
-    extraGroups = [ "wheel" ];
+    extraGroups = [
+      "wheel"
+      "video"
+      "audio"
+    ];
   };
 
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
 
   xdg.portal = {
     enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-    config.common.default = [ "hyprland" ];
+    extraPortals = [
+      pkgs.xdg-desktop-portal-hyprland
+      pkgs.xdg-desktop-portal-gtk
+    ];
+    config.common.default = [
+      "hyprland"
+      "gtk"
+    ];
   };
 
   programs.zsh.enable = true;
@@ -46,6 +58,8 @@
     xwayland.enable = true;
     withUWSM = true;
   };
+
+  services.upower.enable = true;
 
   services.displayManager.defaultSession = "hyprland-uwsm";
   services.greetd = {
@@ -61,13 +75,13 @@
   programs.uwsm.enable = true;
 
   security.rtkit.enable = true;
-
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+    wireplumber.enable = true;
   };
 
   environment.systemPackages = with pkgs; [ ];
