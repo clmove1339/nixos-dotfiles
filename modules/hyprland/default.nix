c3ee6f0135d1cf5cc4fad033987626f8dd483673
clmove <clmove@xyecoc.com>
2026-01-24 23:55:21 +0300

    formatting...

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -68,21 +68,46 @@ in
         "$mod CTRL, right, workspace, +1"
 
         # Generate Workspace Bindings (Key 1-9 -> WS 1-9, Key 0 -> WS 10)
-        (map (n:
-          let
-            key = toString n;
-            ws = if n == 0 then "10" else key;
-          in [
-            "$mod, ${key}, workspace, ${ws}"
-            "$mod SHIFT, ${key}, movetoworkspace, ${ws}"
+        (map
+          (
+            n:
+            let
+              key = toString n;
+              ws = if n == 0 then "10" else key;
+            in
+            [
+              "$mod, ${key}, workspace, ${ws}"
+              "$mod SHIFT, ${key}, movetoworkspace, ${ws}"
+            ]
+          )
+          [
+            1
+            2
+            3
+            4
+            5
+            6
+            7
+            8
+            9
+            0
           ]
-        ) [ 1 2 3 4 5 6 7 8 9 0 ])
+        )
 
         # Directional Bindings (Focus, Move, Resize)
         (lib.mapAttrsToList (key: dir: [
           "$mod, ${key}, movefocus, ${dir}"
           "$mod SHIFT, ${key}, movewindow, ${dir}"
-          "$mod ALT, ${key}, resizeactive, ${if dir == "l" then "-20 0" else if dir == "r" then "20 0" else if dir == "u" then "0 -20" else "0 20"}"
+          "$mod ALT, ${key}, resizeactive, ${
+            if dir == "l" then
+              "-20 0"
+            else if dir == "r" then
+              "20 0"
+            else if dir == "u" then
+              "0 -20"
+            else
+              "0 20"
+          }"
         ]) directions)
       ];
 
